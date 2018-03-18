ff4f725e2c58b4dc802886496cebdf9f5329b453
Nilesh <agnilesh8199@gmail.com>
2018-03-18 22:55:49 +0530

    updating round score

--- app.js
+++ app.js
@@ -36,10 +36,9 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 document.querySelector('.btn-hold').addEventListener('click',function () {
 
     scores[activePlayer] += roundscores;
-
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     //hitting hold transfers the die to other player
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 100){
         document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display='none';
         document.querySelector('.player-' +activePlayer + '-panel').classList.add('winner');
@@ -81,6 +80,6 @@ function init(){
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.querySelector('name-0').textContent = 'Player 1';
-    document.querySelector('name-1').textContent = 'Player 2';
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
 }
