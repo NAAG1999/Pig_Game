414d464ea421ac5d4f7f52e17d8ff34c3072c113
Nilesh <agnilesh8199@gmail.com>
2018-03-18 22:08:37 +0530

    updating round score

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ document.querySelector('.btn-hold').addEventListener('click',function () {
     if(scores[activePlayer] >= 20){
         document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display='none';
-
+        document.querySelector('.player-' +activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' +activePlayer + '-panel').classList.remove('active');
     }
     else {
         nextPlayer();
